eb43818f2006a10610130e7b674008cf3a2549a0
sarah11qing <sarah11qing@sina.com>
2022-03-27 21:32:10 -0700

    update layout

--- app/src/main/java/com/example/parstagram/MainActivity.kt
+++ app/src/main/java/com/example/parstagram/MainActivity.kt
@@ -1,20 +1,31 @@
 package com.example.parstagram
 
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
-import com.parse.FindCallback
-import com.parse.ParseException
-import com.parse.ParseQuery
-import com.parse.ParseUser
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.core.content.FileProvider
+import com.parse.*
+import java.io.File
 
 /**
  * Let user create a post by taking a photo with their camera
  */
 
 class MainActivity : AppCompatActivity() {
+
+    val CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 1034
+    val photoFileName = "photo.jpg"
+    var photoFile: File? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -29,25 +40,40 @@ class MainActivity : AppCompatActivity() {
             // get the description that they have inputted
             val description = findViewById<EditText>(R.id.etDescription).text.toString()
             val user = ParseUser.getCurrentUser()
-            submitPost(description, user)
+            if (photoFile != null) {
+                submitPost(description, user, photoFile!!)
+            } else {
+                // TODO: print error log message
+                // TODO: show a toast to the user to let them know to take a picture
+                Toast.makeText(this, "Please take a picture!", Toast.LENGTH_SHORT).show()
+            }
         }
 
+        findViewById<Button>(R.id.btnTakepicture).setOnClickListener {
+            // launch camera to let user take picture
+            onLaunchCamera()
+        }
 
         queryPosts()
     }
 
+
     // send a post object to our Parse server
-    fun submitPost(description: String, user: ParseUser) {
+    fun submitPost(description: String, user: ParseUser, photoFile: File) {
         // create the Post object
         val post = Post()
+
         post.setDescription(description)
         post.setUser(user)
+        post.setImage(ParseFile(photoFile))
+
         post.saveInBackground { exception ->
             if (exception != null) {
                 // something has went wrong
                 Log.e(TAG, "Error while saving post")
                 exception.printStackTrace()
                 // TODO: Show a toast to tell user something went wrong with saving post
+                Toast.makeText(this, "Saving post error!", Toast.LENGTH_SHORT).show()
             } else {
                 Log.i(TAG, "Successfully saved post")
                 // TODO: Reset the EditText field to be empty
@@ -55,6 +81,67 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                // by this point we have the camera photo on disk
+                val takenImage = BitmapFactory.decodeFile(photoFile!!.absolutePath)
+                // RESIZE BITMAP, see section below
+                // Load the taken image into a preview
+                val ivPreview: ImageView = findViewById(R.id.ivPicture)
+                ivPreview.setImageBitmap(takenImage)
+            } else { // Result was a failure
+                Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+
+    fun onLaunchCamera() {
+        // create Intent to take a picture and return control to the calling application
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        // Create a File reference for future access
+        photoFile = getPhotoFileUri(photoFileName)
+
+        // wrap File object into a content provider
+        // required for API >= 24
+        // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
+        if (photoFile != null) {
+            val fileProvider: Uri =
+                FileProvider.getUriForFile(this, "com.codepath.fileprovider", photoFile!!)
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider)
+
+            // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+            // So as long as the result is not null, it's safe to use the intent.
+
+            // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
+            // So as long as the result is not null, it's safe to use the intent.
+            if (intent.resolveActivity(packageManager) != null) {
+                // Start the image capture intent to take photo
+                startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE)
+            }
+        }
+    }
+
+    // Returns the File for a photo stored on disk given the fileName
+    fun getPhotoFileUri(fileName: String): File {
+        // Get safe storage directory for photos
+        // Use `getExternalFilesDir` on Context to access package-specific directories.
+        // This way, we don't need to request external read/write runtime permissions.
+        val mediaStorageDir =
+            File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG)
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()) {
+            Log.d(TAG, "failed to create directory")
+        }
+
+        // Return the file target for the photo based on filename
+        return File(mediaStorageDir.path + File.separator + fileName)
+    }
+
 
     // Query for all posts in our server
     fun queryPosts() {
